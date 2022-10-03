fd27802848b6b510ae6f3876a77ed90f46f74261
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-10-03 20:01:57 -0300

    Create node server

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http') 
+
+const PORT = 3000
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200)
+            .end('Home page')
+})
+
+server.listen(PORT)
+      .on('listening', () => console.log(`Server started on port ${PORT}.`))
