cc84de38d8df52d27ab3c5d86c17525efeb915b4
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-10-03 21:40:19 -0300

    implements cluster

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import http from 'node:http'
+import cluster from 'node:cluster'
+import { cpus } from 'node:os'
 
 const PORT = 3000
+const createChildrenProcess = () => {
+    const processLength = cpus().length
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200)
-            .end('Home page')
-})
+    console.log(`\nServer initilized with PID ${process.id}`)
+    console.log(`Creating ${processLength} children process`)
 
-server.listen(PORT)
-      .on('listening', () => console.log(`Server started on port ${PORT}.`))
+    for (let i = 0; i < processLength; i++) {
+        cluster.fork()
+    }
+}
+
+const initChildProcess = () => {
+    const server = http.createServer((request, response) => {
+        response.writeHead(200)
+                .end('Home page')
+    })
+
+    server.listen(PORT)
+        .on('listening', 
+            () => console.log(`\nProcess ${process.pid} initialized on port ${PORT}.`)
+        )
+}
+
+if (cluster.isPrimary) createChildrenProcess()
+else initChildProcess()
